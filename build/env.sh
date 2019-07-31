76dcc807e018339df16f1ed4e2297fbb11c1983a
Petro Sidlovsky <sidlovskyy@gmail.com>
2019-07-31 15:00:07 +0300

    Fix build

--- build/env.sh
+++ build/env.sh
@@ -14,7 +14,7 @@ ethdir="$workspace/src/github.com/AERUMTechnology"
 if [ ! -L "$ethdir/go-aerum" ]; then
     mkdir -p "$ethdir"
     cd "$ethdir"
-    ln -s ../../../../../. go-ethereum
+    ln -s ../../../../../. go-aerum
     cd "$root"
 fi
 
